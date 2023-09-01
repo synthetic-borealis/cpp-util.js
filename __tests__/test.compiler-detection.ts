b03da9dfe35e61538873cfaa25e6f5b4a33f1c62
synthetic-borealis <snip.coloured@gmail.com>
2023-09-01 14:44:54 +0300

    Cleanup tests

--- __tests__/test.compiler-detection.ts
+++ __tests__/test.compiler-detection.ts
@@ -8,9 +8,9 @@ import {
 } from '../src';
 
 describe('Compiler detection', () => {
-  it('Throws CompilerNotFoundError when a specified compiler cannot be found', () => {
+  it('Throws CompilerNotFoundError when a specified compiler cannot be found', async () => {
     const falseCompiler = 'lol';
-    return expect(checkForCompiler(falseCompiler)).rejects.toBeInstanceOf(CompilerNotFoundError);
+    await expect(checkForCompiler(falseCompiler)).rejects.toBeInstanceOf(CompilerNotFoundError);
   });
   it('Detects gcc', () => expect(checkForGcc()).resolves.toBeDefined());
   it('Detects g++', () => expect(checkForGPlus()).resolves.toBeDefined());
